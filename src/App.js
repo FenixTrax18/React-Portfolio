097600b1da1048b760036bc5e9b7dd14217fbecf
FenixTrax18 <60018827+FenixTrax18@users.noreply.github.com>
2020-07-29 22:00:29 -0400

    ..

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import PortfolioPg from "./pages/portfolio";
 import Portfolio from './components/Portfolio';
-import Images from '../src/assets/images';
 
 {/* import images (issue with paths), import node projects, fix FDL, ?pages/routing? */}
 
