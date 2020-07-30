29bcad34a3c5b71128a431d6efc9446da98b5d8e
FenixTrax18 <60018827+FenixTrax18@users.noreply.github.com>
2020-07-29 20:56:00 -0400

    Updates

---
+++ precache-manifest.08056239c6b741b44d3ce9d18bf92124.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80077fc1562b1512c3da204ed4c529a",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "d90a7574328c59724078",
+    "url": "/React-Portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9db0b8b3ba575a8bf83a",
+    "url": "/React-Portfolio/static/css/main.db216f22.chunk.css"
+  },
+  {
+    "revision": "d90a7574328c59724078",
+    "url": "/React-Portfolio/static/js/2.5cf8f464.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Portfolio/static/js/2.5cf8f464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db0b8b3ba575a8bf83a",
+    "url": "/React-Portfolio/static/js/main.ba8e10eb.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "2f519ba2d67e8abbecfe13cbcc0da8bf",
+    "url": "/React-Portfolio/static/media/J&K.2f519ba2.jpg"
+  },
+  {
+    "revision": "975f7b392604096c4cc44301a0dc61a8",
+    "url": "/React-Portfolio/static/media/blueflowers.975f7b39.jpg"
+  }
+]);
