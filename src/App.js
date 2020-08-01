06168817db8f5126cfb2385b7389b2fe5bd941de
FenixTrax18 <60018827+FenixTrax18@users.noreply.github.com>
2020-08-01 09:46:04 -0400

    change browser router to hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import AboutPg from "./pages/about";
 import PortfolioPg from "./pages/portfolio";
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <div>
           <Route exact path="/React-Portfolio/" component={AboutPg} />
-          <Route exact path={process.env.PUBLIC_URL+"/Portfolio/"} component={PortfolioPg} />
+          <Route exact path="/React-Portfolio/Portfolio/" component={PortfolioPg} />
       </div>
     </Router>
   );
