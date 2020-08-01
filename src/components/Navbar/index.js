30a8fd136a6ce424168951385d5e0b702f0d5a1e
FenixTrax18 <60018827+FenixTrax18@users.noreply.github.com>
2020-08-01 14:21:44 -0400

    link issues

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+
 function PortNav() {
   return (
 <Navbar expand="lg">
@@ -10,7 +11,7 @@ function PortNav() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/React-Portfolio/About/">About</Nav.Link>
+            <Nav.Link href="/React-Portfolio/">About</Nav.Link>
             <Nav.Link href="/React-Portfolio/#/Portfolio/">Portfolio</Nav.Link>
             <Nav.Link href="https://github.com/FenixTrax18" target="_blank">GitHub</Nav.Link>
             <Nav.Link href="https://www.linkedin.com/in/juliahopkinsms/" target="_blank">LinkedIn</Nav.Link>
