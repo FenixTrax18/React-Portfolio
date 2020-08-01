8320037ba712f7f95e1ed7c0461dce678856485e
FenixTrax18 <60018827+FenixTrax18@users.noreply.github.com>
2020-07-31 20:58:52 -0400

    link fix 2.0

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Resume from 'https://fenixtrax18.github.io/React-Portfolio/assets/Julia_Hopkins_Resume.pdf'
 
 function PortNav() {
   return (
@@ -14,7 +13,7 @@ function PortNav() {
             <Nav.Link href="/React-Portfolio/portfolio">Portfolio</Nav.Link>
             <Nav.Link href="https://github.com/FenixTrax18" target="_blank">GitHub</Nav.Link>
             <Nav.Link href="https://www.linkedin.com/in/juliahopkinsms/" target="_blank">LinkedIn</Nav.Link>
-            <Nav.Link href="{Resume}" target="_blank">Resume</Nav.Link>
+            <Nav.Link href="https://fenixtrax18.github.io/React-Portfolio/assets/Julia_Hopkins_Resume.pdf" target="_blank">Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
